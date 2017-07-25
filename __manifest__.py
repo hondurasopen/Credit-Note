e92b1efb916eaf76c5cc5661fcf4654214aec086
alejo <megaaleja@gmail.com>
2017-07-25 10:38:14 -0600

    Credit Note and Debit Note

--- __manifest__.py
+++ __manifest__.py
@@ -2,22 +2,20 @@
 {
 "name":"Credit Note",
 "author": "Alejandro Rodriguez, Honduras Open Source",
-"description": "This Module creates Credit Note on Invoices",
+"description": "This Module creates credit Note and debit note on Invoices",
 "category":"Sale",
 "depends":["base",
            "sale",
 		      "account"
 ],
  "data": [
+ 		'security/groups.xml',
+        'security/ir.model.access.csv',
  		"views/nota_debito_view.xml",
  		"views/account_nota_debito.xml",
  		"views/credit_note_inv_view.xml",
 	    "views/account_credit_note.xml",
         ],
-'update_xml' : [
-        'security/groups.xml',
-        'security/ir.model.access.csv'
-],
 "auto_install": False,
 "installable": True,
 }
